1e30a161390cdae21f78ac8dd8fd262c70de3c9b
Pavel Krejcir <pavel@pamsoft.cz>
2019-01-23 21:15:10 +0100

    fixed bat compile files for Windows

--- compile32.bat
+++ compile32.bat
@@ -1,2 +1,3 @@
 if "%1" == "a" mingw32-make -fmakefile.mingw BUILDDIR=Build32 clean
+mingw32-make -fmakefile.mingw PREFIX=i686-w64-mingw32- TOOLPFX=i686-w64-mingw32- BUILDDIR=Build32 SUFFIX=32 libdxflib.a 2> libdxflib.log
 mingw32-make -fmakefile.mingw PREFIX=i686-w64-mingw32- TOOLPFX=i686-w64-mingw32- BUILDDIR=Build32 SUFFIX=32 SteamCAD 2> SteamCAD.log
--- compile64.bat
+++ compile64.bat
@@ -1,2 +1,3 @@
 if "%1" == "a" mingw32-make -fmakefile.mingw BUILDDIR=Build64 clean
+mingw32-make -fmakefile.mingw PREFIX=x86_64-w64-mingw32- BUILDDIR=Build64 SUFFIX=64 libdxflib.a 2> libdxflib.log
 mingw32-make -fmakefile.mingw PREFIX=x86_64-w64-mingw32- BUILDDIR=Build64 SUFFIX=64 SteamCAD 2> SteamCAD.log
